f5b0ece95a4b6d146cad00fed466020b48fcaafa
Yamca <asamalin2004@gmail.com>
2026-02-28 21:24:26 +0500

    nothing serious

--- src/main/java/GamieBot/model/games/IGame.java
+++ src/main/java/GamieBot/model/games/IGame.java
@@ -2,7 +2,7 @@ package GamieBot.model.games;
 
 
 public interface IGame {
-    public void run();
+    public void initGame(int firstPlayer, int secondPlayer);
     public void makeMove(int playerNum, String action);
     public String getInfoForPlayer(int playerNum);
 }
--- src/main/java/GamieBot/model/games/TicTacToe.java
+++ src/main/java/GamieBot/model/games/TicTacToe.java
@@ -3,11 +3,39 @@ package GamieBot.model.games;
 
 public class TicTacToe implements IGame {
     @Override
-    public void run() {}
+    public void initGame(int firstPlayer, int secondPlayer) {
+        state = "=======\n| | | |\n=======\n| | | |\n=======\n| | | |\n=======";
+        player1 = firstPlayer;
+        player2 = secondPlayer;
+        isFirstPlayerMove = true;
+    }
+
+    /*
+    =======
+    | | | |
+    =======
+    | | | |
+    =======
+    | | | |
+    =======
+    */
 
     @Override
-    public void makeMove(int playerNum, String action) {}
+    public void makeMove(int playerNum, String action) {
+        if (playerNum == player1) {
+            if (isFirstPlayerMove) {
+                
+            }
+        }
+    }
 
     @Override
-    public String getInfoForPlayer(int playerNum) {return "aboba";}
+    public String getInfoForPlayer(int playerNum) {
+
+        return "aboba";
+    }
+
+    private String state;
+    private int player1, player2;
+    private boolean isFirstPlayerMove;
 }
